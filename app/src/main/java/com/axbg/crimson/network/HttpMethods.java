ae3cfc2c298c27289323b5eed4292fce7939535a
axbg <bisagalexstefan@gmail.com>
2020-06-09 23:44:37 +0300

    Added HTTP calls handler

---
+++ app/src/main/java/com/axbg/crimson/network/HttpMethods.java
@@ -0,0 +1,8 @@
+package com.axbg.crimson.network;
+
+interface HttpMethods {
+    String GET_METHOD = "GET";
+    String POST_METHOD = "POST";
+    String PUT_METHOD = "PUT";
+    String DELETE_METHOD = "DELETE";
+}
